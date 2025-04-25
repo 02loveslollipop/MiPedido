e388dd6152b31b0ce0716e02c5fd95b0e9a911a1
02loveslollipop <mmonsalve2005@gmail.com>
2025-04-25 14:30:17 -0500

    feat: Initialize Android projet for client app

---
+++ appCliente/app/src/main/java/uk/app02loveslollipop/mipedido/cliente/MainActivity.kt
@@ -0,0 +1,47 @@
+package uk.app02loveslollipop.mipedido.cliente
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uk.app02loveslollipop.mipedido.cliente.ui.theme.MiPedidoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiPedidoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiPedidoTheme {
+        Greeting("Android")
+    }
+}
